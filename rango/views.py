05316a794e2bd0965e07bf7d77613ff72a596c5a
Panagiotis916 <panayiotisscorps@hotmail.com>
2016-03-14 15:22:35 +0000

    Fixed Viewed Documents Button and Updated the viewed_documents.html

--- rango/views.py
+++ rango/views.py
@@ -78,33 +78,30 @@ def category(request, category_name_slug):
     return render(request, 'rango/category.html', context_dict)
 
 def viewed_documents(request, category_name_slug):
-    # Create a context dictionary which we can pass to the template rendering engine.
-    context_dict = {}
-
     try:
-        # Can we find a category name slug with the given name?
-        # If we can't, the .get() method raises a DoesNotExist exception.
-        # So the .get() method returns one model instance or raises an exception.
-        category = Category.objects.get(slug=category_name_slug)
-        context_dict['category_name'] = category.name
-        context_dict['category_name_slug'] = category.slug
-
-        # Retrieve all of the associated pages.
-        # Note that filter returns >= 1 model instance.
-        pages = Page.objects.filter(category=category)
-
-        # Adds our results list to the template context under name pages.
-        context_dict['pages'] = pages
-        # We also add the category object from the database to the context dictionary.
-        # We'll use this in the template to verify that the category exists.
-        context_dict['category'] = category
+        cat = Category.objects.get(slug=category_name_slug)
     except Category.DoesNotExist:
-        # We get here if we didn't find the specified category.
-        # Don't do anything - the template displays the "no category" message for us.
-        pass
+        cat = None
 
-    # Go render the response and return it to the client.
+    if request.method == 'POST':
+        form = PageForm(request.POST)
+        if form.is_valid():
+            if cat:
+                page = form.save(commit=False)
+                page.category = cat
+                page.views = 0
+                page.save()
+                # probably better to use a redirect here.
+                return category(request, category_name_slug)
+        else:
+            print form.errors
+    else:
+        form = PageForm()
+
+    context_dict = {'form': form, 'category': cat}
+
     return render(request, 'rango/viewed_documents.html', context_dict)
+
 
 
 def add_category(request):
