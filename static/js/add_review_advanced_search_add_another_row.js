9663531b558bcc48c53aeb2eeb0b2d0c26aca5a0
Alex Irikov <Alex Irikov>
2016-03-20 18:01:49 +0000

    Started on the advanced query, popup, buttons, no positions, not passing them correctly yet

---
+++ static/js/add_review_advanced_search_add_another_row.js
@@ -0,0 +1,13 @@
+//function to add the input from the advanced search query (not or nothing in the beginning), AND/OR after it
+$(document).ready(function(){
+    $('#plus_button').click(function(){
+    //    var toAdd = $('input[name=query_string_advanced]').val();
+    //    var select_not = $("fop_2").val;
+      alert( $("fop_2").va);
+    //
+    //    $('.list').append('<div class="item">' + toAdd + '</div>');
+    //});
+    //$(document).on('click','.item',function(){
+    //    $(this).remove();
+    });
+});
