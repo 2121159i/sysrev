04cc8528f665d6349ff108d99e1800b100940218
Alex Irikov <Alex Irikov>
2016-03-20 18:58:51 +0000

    Fixed advanced search, still have to pass it on

--- static/js/add_review_advanced_search_add_another_row.js
+++ static/js/add_review_advanced_search_add_another_row.js
@@ -2,9 +2,10 @@
 $(document).ready(function(){
     $('#plus_button').click(function(){
         var toAdd = $('input[name=query_string_advanced]').val();
-        var selectNot = $("#not_selector option:selected").text();
-        var selectAndOr = $("#and_or_selector option:selected").text();
-        $('.list').append('<div class="item">' + selectNot + " " + toAdd + " " + selectAndOr + '</div>');
+        var select_not = $("#not_selector option:selected").text();
+        var select_and_or = $("#and_or_selector option:selected").text();
+
+        $('.list').append('<div class="item">' + select_not + " " + toAdd + " " + select_and_or + '</div>');
     });
     $(document).on('click','.item',function(){
         $(this).remove();
