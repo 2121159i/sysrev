d13d526ae0a40449b3043bb060d3f0ba964d4198
domantasjurkus <domantas.jurkus@gmail.com>
2016-03-21 00:05:59 +0000

    Updated settings

--- sysRevu/settings.py
+++ sysRevu/settings.py
@@ -1,36 +1,16 @@
-"""
-Django settings for newProject project.
 
-For more information on this file, see
-https://docs.djangoproject.com/en/1.7/topics/settings/
-
-For the full list of settings and their values, see
-https://docs.djangoproject.com/en/1.7/ref/settings/
-"""
-
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/1.7/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'f6qav0$e=f3pjpeot@+^r&@-j1#y5$=w16(no*zm)o!cx1%_^p'
 
-# SECURITY WARNING: don't run with debug turned on in production!
+
 DEBUG = True
-
 TEMPLATE_DEBUG = True
-
 ALLOWED_HOSTS = []
 
 TEMPLATE_PATH = os.path.join(BASE_DIR, 'templates')
 TEMPLATE_DIRS = (
-    # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
     TEMPLATE_PATH,
 )
 
@@ -43,7 +23,7 @@ STATICFILES_DIRS = (
     STATIC_PATH,
 )
 
-# Application definition
+
 
 INSTALLED_APPS = (
     'django.contrib.admin',
@@ -53,15 +33,12 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'sysrev',
-    'registration',
-    'bootstrap_toolkit',
 )
-REGISTRATION_OPEN = True                # If True, users can register
-ACCOUNT_ACTIVATION_DAYS = 7     # One-week activation window; you may, of course, use a different value.
-REGISTRATION_AUTO_LOGIN = True  # If True, the user will be automatically logged in.
-LOGIN_REDIRECT_URL = '/sysrev/'  # The page you want users to arrive at after they successful log in
-LOGIN_URL = '/accounts/login/'  # The page users are directed to if they are not logged in, and are trying to access pages requiring authentication
-#LOGIN_URL = '/sysrev/login/'
+REGISTRATION_OPEN = True
+ACCOUNT_ACTIVATION_DAYS = 7
+REGISTRATION_AUTO_LOGIN = True
+LOGIN_REDIRECT_URL = '/sysrev/'
+LOGIN_URL = '/accounts/login/'
 
 PASSWORD_HASHERS = (
 'django.contrib.auth.hashers.PBKDF2PasswordHasher',
@@ -83,8 +60,6 @@ ROOT_URLCONF = 'sysRevu.urls'
 WSGI_APPLICATION = 'sysRevu.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/1.7/ref/settings/#databases
 
 DATABASES = {
     'default': {
@@ -93,21 +68,14 @@ DATABASES = {
         }
 }
 
-# Internationalization
-# https://docs.djangoproject.com/en/1.7/topics/i18n/
+
 
 LANGUAGE_CODE = 'en-us'
-
 TIME_ZONE = 'UTC'
-
 USE_I18N = True
-
 USE_L10N = True
-
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 STATIC_URL = '/static/'
