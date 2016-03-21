b9cd066f9ee5e22b379fb1646adee40f52d58b6d
domantasjurkus <domantas.jurkus@gmail.com>
2016-03-21 12:41:14 +0000

    Deleted migration history

--- sysrev/migrations/0001_initial.py
+++ sysrev/migrations/0001_initial.py
@@ -2,35 +2,79 @@
 from __future__ import unicode_literals
 
 from django.db import models, migrations
+import django.utils.timezone
+import datetime
+from django.conf import settings
 
 
 class Migration(migrations.Migration):
 
     dependencies = [
+        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
     ]
 
     operations = [
         migrations.CreateModel(
-            name='Category',
+            name='Paper',
             fields=[
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
-                ('name', models.CharField(unique=True, max_length=128)),
+                ('pubmed_id', models.IntegerField()),
+                ('title', models.CharField(default=b'', max_length=128)),
+                ('authors', models.CharField(default=b'', max_length=128)),
+                ('abstract', models.CharField(default=b'', max_length=4096)),
+                ('paper_url', models.URLField(default=None, null=True, blank=True)),
+                ('date', models.DateTimeField(default=datetime.datetime.now, auto_now_add=True)),
+                ('abstract_rev', models.NullBooleanField(default=None)),
+                ('document_rev', models.NullBooleanField(default=None)),
             ],
             options={
             },
             bases=(models.Model,),
         ),
         migrations.CreateModel(
-            name='Page',
+            name='Query',
             fields=[
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
-                ('title', models.CharField(max_length=128)),
-                ('url', models.URLField()),
-                ('views', models.IntegerField(default=0)),
-                ('category', models.ForeignKey(to='sysrev.Category')),
+                ('string', models.CharField(max_length=128)),
             ],
             options={
             },
             bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='Researcher',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('user', models.OneToOneField(to=settings.AUTH_USER_MODEL)),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='Review',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('title', models.CharField(max_length=128)),
+                ('description', models.CharField(max_length=128)),
+                ('date_started', models.DateTimeField(default=django.utils.timezone.now)),
+                ('query_string', models.CharField(max_length=128)),
+                ('user', models.ForeignKey(to='sysrev.Researcher')),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+        migrations.AddField(
+            model_name='query',
+            name='review',
+            field=models.ForeignKey(to='sysrev.Review', null=True),
+            preserve_default=True,
+        ),
+        migrations.AddField(
+            model_name='paper',
+            name='review',
+            field=models.ForeignKey(default=None, to='sysrev.Review'),
+            preserve_default=True,
         ),
     ]
