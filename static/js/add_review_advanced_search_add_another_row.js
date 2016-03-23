6eafeb8f6a9fb00dd07a424559510e3e34d03a32
Alex Irikov <airikov95@gmail.com>
2016-03-23 16:45:42 +0000

    Fixed adding another row in the Advanced Query Builder

--- static/js/add_review_advanced_search_add_another_row.js
+++ static/js/add_review_advanced_search_add_another_row.js
@@ -1,6 +1,6 @@
 //function to add the input from the advanced search query (not or nothing in the beginning), AND/OR after it
 $(document).ready(function(){
-    $('#plus_button').click(function(){
+    $('#button_add_new_row').click(function(){
         var toAdd = $('input[name=query_string_advanced]').val();
 
         var selectNot = $("#not_selector option:selected").text();
